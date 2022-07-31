cfa76b49081fc3b94522e645659cf65621c1461b
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-07-31 20:20:06 +0500

    video controller started

---
+++ Controllers/VideoController.cs
@@ -0,0 +1,27 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using YouTube.Models;
+
+namespace YouTube.Controllers
+{
+   public class VideoController : Controller
+   {
+      private readonly ILogger<VideoController> _logger;
+
+      public VideoController(ILogger<VideoController> logger)
+      {
+         _logger = logger;
+      }
+
+      public IActionResult Index()
+      {
+         return View();
+      }
+
+      [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+      public IActionResult Error()
+      {
+         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+      }
+   }
+}
